f29d228ab6e5e856e44a1f13d5438c8fb6f0a431
adityadeshpande2603 <119919235+adityadeshpande2603@users.noreply.github.com>
2025-01-09 21:44:11 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,10 @@ dotenv.config();
 const app=express();
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({origin:process.env.CLIENT_URL,credentials:true}));
+app.use(cors({
+    origin: "https://vastuu-frontend.onrender.com", // Your frontend's URL
+    credentials: true,
+}));
 
 
 
